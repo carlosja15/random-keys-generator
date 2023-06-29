a848a32e48400f878296558110563985f5a68e90
carlosja15 <carlosja_15@hotmail.com>
2023-06-29 01:44:27 -0500

    create project

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+
+var keySize = ReadKeySize();
+var secretKey = GenerateRandomKey(keySize);
+
+Console.WriteLine(secretKey);
+
+int ReadKeySize()
+{
+    Console.WriteLine("Ingrese el tamaño de la clave en bytes: ");
+    string input = Console.ReadLine();
+    int keySize;
+    while (!int.TryParse(input, out keySize))
+    {
+        Console.WriteLine("Tamaño de la clave no válido. Intente nuevamente");Console.Write("Ingrese el tamaño de la clave en bytes: ");
+        input = Console.ReadLine();
+    }
+    
+    return keySize;
+}
+
+string GenerateRandomKey(int keySize)
+{
+    byte[] buffer = new byte[keySize];
+    using var rng = RandomNumberGenerator.Create();
+    rng.GetBytes(buffer);
+    return Convert.ToBase64String(buffer);
+}
